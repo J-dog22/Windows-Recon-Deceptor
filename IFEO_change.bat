eb6d5bf2c3042ebfeb27741341199d9985bd6c9f
J-dog22 <57235683+J-dog22@users.noreply.github.com>
2021-05-06 17:39:29 -0400

    Delete IFEO_change.bat

--- IFEO_change.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-REM This batch script will modify the Image File Execution Options registry keys to intercept the execution of a program.
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Net.exe" /v Debugger /d "C:\Users\IEUser\Desktop\repos\Net\x64\Debug\Net.exe" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\whoami.exe" /v Debugger /d "C:\Users\IEUser\Desktop\repos\whoami\x64\Debug\whoami.exe" /f
-reg add "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\ipconfig.exe" /v Debugger /d "C:\Users\IEUser\Desktop\repos\ipconfig\x64\Debug\ipconfig.exe" /f
-Echo "Keys Modified!"
