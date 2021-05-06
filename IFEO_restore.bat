930180e4031958fe222dfa462f86fdc8c5d86f93
J-dog22 <57235683+J-dog22@users.noreply.github.com>
2021-05-06 17:39:47 -0400

    Delete IFEO_restore.bat

--- IFEO_restore.bat
+++
@@ -1,6 +0,0 @@
-@ECHO OFF
-REM This batch script will restore the Image File Execution Options registry keys that intercept the execution of a program.
-reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\Net.exe" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\whoami.exe" /f
-reg delete "HKLM\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Image File Execution Options\ipconfig.exe" /f
-Echo "Original Keys Restored!"
